ad6e927a1c6f44801a86d51c94fe99da8d652bb9
Salvin <65126223+salvinmirza@users.noreply.github.com>
2023-03-30 21:28:47 -0400

    hacker rank first code commit
    hacker rank first code commit

---
+++ javaStdInandStdOutI.java
@@ -0,0 +1,19 @@
+package hackerrank;
+import java.util.*;
+
+
+public class javaStdInandStdOutI {
+	
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+      
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+
+}
---
+++ welcomToJava.java
@@ -0,0 +1,12 @@
+package hackerrank;
+import java.util.*;
+
+
+public class welcomToJava {
+	
+	public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+
+}
